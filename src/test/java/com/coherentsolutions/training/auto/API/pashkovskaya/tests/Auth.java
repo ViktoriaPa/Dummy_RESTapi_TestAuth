4e6a3eb4f6bfc6744bcc06452d8d7e93f4e81ffc
viktoriapashkovskaya <vika.vabishchevich.96@mail.ru>
2023-08-14 22:16:22 +0200

    added Jackson

--- src/test/java/com/coherentsolutions/training/auto/API/pashkovskaya/tests/Auth.java
+++ src/test/java/com/coherentsolutions/training/auto/API/pashkovskaya/tests/Auth.java
@@ -4,23 +4,29 @@ import com.coherentsolutions.training.auto.API.pashkovskaya.model.TokenRes;
 import com.coherentsolutions.training.auto.API.pashkovskaya.model.User;
 import com.coherentsolutions.training.auto.API.pashkovskaya.util.PropertiesFileReader;
 import com.coherentsolutions.training.auto.API.pashkovskaya.base.BaseTest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.testng.Assert.assertNotNull;
 
 public class Auth extends BaseTest {
+    ObjectMapper objectMapper = new ObjectMapper();
     @Test
-    public void testAuthorizationIsSuccessful(){
+    public void testAuthorizationIsSuccessful() throws JsonProcessingException {
         User user = new User(PropertiesFileReader.getUsername(), PropertiesFileReader.getPassword());
 
-        TokenRes token = given()
+        String resBodyJson = given()
                 .when()
                 .body(user)
                 .post("auth/login")
                 .then()
-                .extract().body().jsonPath().getObject("token", TokenRes.class);
+                .extract().body().asPrettyString();
 
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        TokenRes token = objectMapper.readValue(resBodyJson, TokenRes.class);
         assertNotNull(token, "Token is null");
     }
 }
