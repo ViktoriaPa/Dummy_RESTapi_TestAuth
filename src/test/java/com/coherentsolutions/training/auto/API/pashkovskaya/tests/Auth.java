4984a1fd7ee069568078613c57f141a7696af90e
viktoriapashkovskaya <vika.vabishchevich.96@mail.ru>
2023-08-15 13:50:15 +0200

    made objectMapper a global variable

--- src/test/java/com/coherentsolutions/training/auto/API/pashkovskaya/tests/Auth.java
+++ src/test/java/com/coherentsolutions/training/auto/API/pashkovskaya/tests/Auth.java
@@ -5,15 +5,12 @@ import com.coherentsolutions.training.auto.API.pashkovskaya.model.User;
 import com.coherentsolutions.training.auto.API.pashkovskaya.util.PropertiesFileReader;
 import com.coherentsolutions.training.auto.API.pashkovskaya.base.BaseTest;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.testng.Assert.assertNotNull;
 
 public class Auth extends BaseTest {
-    ObjectMapper objectMapper = new ObjectMapper();
     @Test
     public void testAuthorizationIsSuccessful() throws JsonProcessingException {
         User user = new User(PropertiesFileReader.getUsername(), PropertiesFileReader.getPassword());
@@ -25,7 +22,6 @@ public class Auth extends BaseTest {
                 .then()
                 .extract().body().asPrettyString();
 
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         TokenRes token = objectMapper.readValue(resBodyJson, TokenRes.class);
         assertNotNull(token, "Token is null");
     }
